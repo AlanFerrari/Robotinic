b5d9f72146c1ed3e7d882d24d3b1e493f14311d5
alanferrari <alanferrari33@gmail.com>
2022-03-21 17:57:50 -0300

    Código do Sensor Infravermelho

--- Blink2/Blink2.ino
+++ Blink2/Blink2.ino
@@ -1,10 +1,13 @@
 void setup() {
   pinMode(13, OUTPUT);
   pinMode(2, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   int status_botao = digitalRead(2);
+  Serial.print("status do sensor: ");
+  Serial.println(status_botao);
   if(status_botao == 1){
     digitalWrite(13, 1);  
   }
